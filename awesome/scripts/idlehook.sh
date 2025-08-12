791aec848e0a2b46adff1948aa68fafb09095172
Nil Omniscient <nilomniscient@gmail.com>
2025-08-11 22:08:33 -0400

    Swapping xidlehook to xautolock

--- awesome/scripts/idlehook.sh
+++ awesome/scripts/idlehook.sh
@@ -1,19 +1,2 @@
 #!/usr/bin/env bash
-
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
-# Run xidlehook
-xidlehook \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 10 \
-    'betterscreenlock -l' \
-    '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
-    'systemctl suspend' \
-    ''
+xautolock -time 5 -locker "betterlockscreen -l" -killtime 10 -killer "systemctl suspend" &
