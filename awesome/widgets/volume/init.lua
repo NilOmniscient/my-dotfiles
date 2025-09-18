90f841f265546ca72cfb8b6889cbe5a1272a3f85
Nil Omniscient <nilomniscient@gmail.com>
2025-09-18 06:58:17 -0400

    Adding some extra bits to the volume control widget (WIP)

--- awesome/widgets/volume/init.lua
+++ awesome/widgets/volume/init.lua
@@ -10,3 +10,21 @@ local volume_max = "󰕾"
 
 local low_threshold = 33;
 local med_threshold = 66;
+
+local current_volume = 100;
+
+local LIST_DEVICES_CMD = [[sh -c "pacmd list-sinks; pacmd list-sources"]]
+
+local function GET_VOLUME_CMD(device, type)
+  return "pactl get-" .. type .. "-volume " .. device
+end
+local function INC_VOLUME_CMD(device, type, step)
+  current_volume = current_volume + step
+  if current_volume > 100 then current_volume = 100 end
+  return "pactl set-" .. type .. "-volume " .. device .. " " .. current_volume
+end
+local function DEC_VOLUME_CMD(device, type, step)
+  current_volume = current_volume - step
+  if current_volume < 0 then current_volume = 0 end
+  return "pactl set-" .. type .. "-volume " .. device .. " " .. current_volume
+end
