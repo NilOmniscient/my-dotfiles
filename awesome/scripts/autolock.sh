ce1d0f903d971fec5f2ad128e2c26ab6ff039474
Nil Omniscient <nilomniscient@gmail.com>
2025-11-24 23:33:19 -0500

    Rolling back the xidlehook to use xautolock

--- awesome/scripts/autolock.sh
+++ awesome/scripts/autolock.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-if command -v $HOME/.cargo/bin/xidlehook &>/dev/null
-then
-  killall xidlehook
-  $HOME/.cargo/bin/xidlehook --detect-sleep \
-    --not-when-audio \
-    --not-when-fullscreen \
-    --timer 600 'betterlockscreen -l' '' \
-    --socket /tmp/xidle.sock
-else
-  xautolock -time 5 \
-    -locker 'betterlockscreen -l' \
-    -detectsleep
-fi
+xautolock -time 5 \
+  -locker 'betterlockscreen -l' \
+  -detectsleep
--- awesome/scripts/locknow.sh
+++ awesome/scripts/locknow.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if command -v $HOME/.cargo/bin/xidlehook &>/dev/null
-then
-  $HOME/.cargo/bin/xidlehook-client --socket /tmp/xidle.sock control --action trigger --timer 0
-else
-  xautolock -locknow
-fi
+xautolock -locknow
