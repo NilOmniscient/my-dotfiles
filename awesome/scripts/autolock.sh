0d570f8df7cc958e3eadd8e134a5d4c0eef8ac0b
Nil Omniscient <nilomniscient@gmail.com>
2025-11-25 09:44:23 -0500

    Adding back xidlehook

--- awesome/scripts/autolock.sh
+++ awesome/scripts/autolock.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-xautolock -time 5 \
-  -locker 'betterlockscreen -l' \
-  -detectsleep
+if command -v $HOME/.cargo/bin/xidlehook &>/dev/null
+then
+  killall xidlehook
+  $HOME/.cargo/bin/xidlehook --detect-sleep \
+    --not-when-audio \
+    --not-when-fullscreen \
+    --timer 600 'betterlockscreen -l' '' \
+    --socket /tmp/xidle.sock
+else
+  xautolock -time 5 \
+    -locker 'betterlockscreen -l' \
+    -detectsleep
+fi
--- awesome/scripts/locknow.sh
+++ awesome/scripts/locknow.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-xautolock -locknow
+if command -v $HOME/.cargo/bin/xidlehook &>/dev/null
+then
+  $HOME/.cargo/bin/xidlehook-client --socket /tmp/xidle.sock control --action trigger --timer 0
+else
+  xautolock -locknow
+fi
