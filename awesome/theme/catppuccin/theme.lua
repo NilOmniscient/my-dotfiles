6946f5ddbad32eb048a537979e7403f26a4431df
Nil Omniscient <nilomniscient@gmail.com>
2026-03-11 15:56:33 -0400

    Fixing wallpaper

--- awesome/theme/catppuccin/theme.lua
+++ awesome/theme/catppuccin/theme.lua
@@ -110,7 +110,7 @@ theme.titlebar_maximized_button_normal_active = themes_path .. "default/titlebar
 theme.titlebar_maximized_button_focus_active = themes_path .. "default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = themes_path .. "default/background.png"
-theme.wallpaper = gears.filesystem.get_configuration_dir() .. "theme/catppuccin/assets/wallpaper.jpeg"
+theme.wallpaper = gears.filesystem.get_configuration_dir() .. "theme/catppuccin/assets/wallpaper.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
