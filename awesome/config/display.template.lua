d02c8cd0b4521721106ee97450aa67ff932b4dfe
Nil Omniscient <nilomniscient@gmail.com>
2026-04-16 12:39:31 -0400

    Updating the display template

--- awesome/config/display.template.lua
+++ awesome/config/display.template.lua
@@ -1,20 +1,35 @@
 -- Only matters for somewm.
 local is_somewm = awesome.release == "somewm"
 if is_somewm then
+	-- See if there's an external connected.
+	local builtin_only = true
+	for o in output do
+		if o.name ~= "eDP-1" then
+			builtin_only = false
+		end
+	end
+	if builtin_only then
+		return
+	end
 	-- Set up monitors.
-	-- First, handle DisplayPort-1
-	output.get_by_name("DisplayPort-1").mode = { width = 1920, height = 1080, refresh = 60 }
-	output.get_by_name("DisplayPort-1").transform = "270"
-	output.get_by_name("DisplayPort-1").position = { x = 0, y = 0 }
+	-- First, handle DP-1
+	output.get_by_name("DP-10").mode = { width = 1920, height = 1080, refresh = 60 }
+	output.get_by_name("DP-10").position = { x = 0, y = 0 }
 
-	-- Then, DisplayPort-2
-	output.get_by_name("DisplayPort-2").mode = { width = 1920, height = 1080, refresh = 60 }
-	output.get_by_name("DisplayPort-2").position = { x = 1080, y = 440 }
+	-- Then, DP-2
+	output.get_by_name("DP-9").mode = { width = 1920, height = 1080, refresh = 60 }
+	output.get_by_name("DP-9").position = { x = 1920, y = 0 }
 
-	-- Finally, HDMI-A-0
-	output.get_by_name("HDMI-A-0").mode = { width = 1920, height = 1080, refresh = 60 }
-	output.get_by_name("HDMI-A-0").transform = "90"
-	output.get_by_name("HDMI-A-0").position = { x = 3000, y = 0 }
+	-- Finally, disable eDP
+	output.get_by_name("eDP-1").enabled = false
+
+	local primary_output = output.get_by_name("DP-9")
+	for s in screen do
+		if s.output == primary_output then
+			screen.primary = s
+		end
+	end
 end
 return
 -- TODO: Flag this file as "Dirty" in git so updates don't change it after initial commit
+
