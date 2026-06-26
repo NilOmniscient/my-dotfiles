8725d6823cfd76467798ae872a65cf9f382c3b57
Nil Omniscient <nilomniscient@gmail.com>
2026-06-26 12:30:26 -0400

    Adding an autorefresh after lazygit closes

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+vim.api.nvim_create_autocmd("TermClose", {
+  pattern = "*lazygit",
+  callback = function()
+    if package.loaded["nvim-tree.api"] then
+      require("nvim-tree.api").tree.reload()
+    end
+  end,
+})
