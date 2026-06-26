af84bd95459cdaf44f7c564ed3d517d2b3cf0e47
Nil Omniscient <nilomniscient@gmail.com>
2026-06-26 12:43:37 -0400

    Make LSP restart on Lazygit updates

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -2,6 +2,7 @@ require "nvchad.autocmds"
 vim.api.nvim_create_autocmd("TermClose", {
   pattern = "*lazygit",
   callback = function()
+    vim.cmd("lsp restart")
     if package.loaded["nvim-tree.api"] then
       require("nvim-tree.api").tree.reload()
     end
