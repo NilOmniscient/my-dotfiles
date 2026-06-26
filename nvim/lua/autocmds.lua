eabf98ef74c5835fc4f5b3bbb43c225e0ee09b2d
Nil Omniscient <nilomniscient@gmail.com>
2026-06-26 12:52:11 -0400

    Trying an alternate callback

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,9 +1,9 @@
 require "nvchad.autocmds"
-vim.api.nvim_create_autocmd("TermClose", {
-  pattern = "*lazygit",
-  callback = function()
-    if package.loaded["nvim-tree.api"] then
-      require("nvim-tree.api").tree.reload()
-    end
-  end,
-})
+
+local lazygit_closed = function()
+  if package.loaded["nvim-tree.api"] then
+    require("nvim-tree.api").tree.reload()
+  end
+  vim.cmd("lsp restart")
+end
+vim.g.lazygit_on_exit_callback = lazygit_closed
