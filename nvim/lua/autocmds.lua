ac3b1f147d240d49da6d3bf8de54c493851fc0dd
Nil Omniscient <nilomniscient@gmail.com>
2026-07-14 13:30:08 -0400

    Swapping to LazyVim

--- nvim/lua/autocmds.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.autocmds"
-
-local lazygit_closed = function()
-  if package.loaded["nvim-tree.api"] then
-    require("nvim-tree.api").tree.reload()
-  end
-  vim.cmd("lsp restart")
-end
-vim.g.lazygit_on_exit_callback = lazygit_closed
