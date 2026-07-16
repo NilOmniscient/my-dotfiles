9b5363055f374f222f1a4a8d365c850ed35544e5
Nil Omniscient <nilomniscient@gmail.com>
2026-07-16 15:25:13 -0400

    Adding an autocmd for lazygit

--- nvim_new/lua/config/autocmds.lua
+++ nvim_new/lua/config/autocmds.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+-- Force neo-tree to refresh after lazygit closes
+vim.api.nvim_create_autocmd("TermClose", {
+	pattern = "*lazygit",
+	callback = function()
+		if package.loaded["neo-tree.sources.git_status"] then
+			require("neo-tree.sources.git_status").refresh()
+		end
+	end,
+})
