68106164e483d8821702c9bdd826d6739e5db0e4
Nil Omniscient <nilomniscient@gmail.com>
2026-09-08 12:23:00 -0400

    Cleaning up the keybinds files to look nicer, setting up initial bits for the UI and Signals

--- somewm/binds/client/keys.lua
+++ somewm/binds/client/keys.lua
@@ -1,63 +1,73 @@
 local awful = require("awful")
 local client = require("client")
-local gears = require("gears")
 local mod = require("binds.mod")
 local modkey = mod.modkey
-local ctrl = mod.ctrl
 local shift = mod.shift
-local alt = mod.alt
+
+local helper = require("binds.helpers").table_to_bindings
+
+local client_helpers = {
+	toggle_fullscreen = function(c)
+		c.fullscreen = not c.fullscreen
+		c:raise()
+	end,
+	close_client = function(c)
+		c:kill()
+	end,
+	toggle_floating = function(c)
+		c.floating = not c.floating
+	end,
+	toggle_maximize = function(c)
+		c.maximized = not c.maximized
+	end,
+	toggle_ontop = function(c)
+		c.ontop = not c.ontop
+	end,
+	snap_right = function(c)
+		local f = awful.placement.scale + awful.placement.right + awful.placement.maximize_vertically
+		f(c, { honor_workarea = true, to_percent = 0.5 })
+	end,
+	snap_left = function(c)
+		local f = awful.placement.scale + awful.placement.left + awful.placement.maximize_vertically
+		f(c, { honor_workarea = true, to_percent = 0.5 })
+	end,
+	snap_top = function(c)
+		local f = awful.placement.scale + awful.placement.top + awful.placement.maximize_horizontally
+		f(c, { honor_workarea = true, to_percent = 0.5 })
+	end,
+	snap_bottom = function(c)
+		local f = awful.placement.scale + awful.placement.bottom + awful.placement.maximize_horizontally
+		f(c, { honor_workarea = true, to_percent = 0.5 })
+	end,
+	move_to_left = function(c)
+		local s = c.screen:get_next_in_direction("left")
+		c:move_to_screen(s)
+	end,
+	move_to_right = function(c)
+		local s = c.screen:get_next_in_direction("right")
+		c:move_to_screen(s)
+	end,
+}
+
+local client_bindings = {
+	-- Client State
+	{ { modkey }, "a", client_helpers.toggle_fullscreen, "Toggle Fullscreen", "client" },
+	{ { modkey }, "q", client_helpers.close_client, "Close Client", "client" },
+	{ { modkey }, "space", client_helpers.toggle_floating, "Toggle Floating", "client" },
+	{ { modkey }, "m", client_helpers.toggle_maximize, "(un)maximize", "client" },
+	{ { modkey }, "t", client_helpers.toggle_ontop, "Toggle keep on top", "client" },
+
+	-- Client Position
+	{ { modkey }, "Right", client_helpers.snap_right, "Snap to right edge", "floating client" },
+	{ { modkey }, "Left", client_helpers.snap_left, "Snap to left edge", "floating client" },
+	{ { modkey }, "Up", client_helpers.snap_top, "Snap to top edge", "floating client" },
+	{ { modkey }, "Down", client_helpers.snap_bottom, "Snap to bottom edge", "floating client" },
+
+	-- Client screen management
+	{ { modkey, shift }, "Left", client_helpers.move_to_left, "Move left one screen", "client" },
+	{ { modkey, shift }, "Right", client_helpers.move_to_right, "Move Right one screen", "client" },
+}
 
 client.connect_signal("request::default_keybindings", function()
-	awful.keyboard.append_client_keybindings({
-		-- Client State
-		awful.key({ modkey }, "a", function(c)
-			c.fullscreen = not c.fullscreen
-			c:raise()
-		end, { description = "fullscreen", group = "client" }),
-		awful.key({ modkey }, "q", function(c)
-			c:kill()
-		end, { description = "close", group = "client" }),
-		awful.key({ modkey }, "space", function(c)
-			c.floating = not c.floating
-		end, { description = "toggle floating", group = "client" }),
-		awful.key({ modkey }, "m", function(c)
-			c.maximized = not c.maximized
-			c:raise()
-		end, { description = "(un)maximize", group = "client" }),
-		awful.key({ modkey, ctrl }, "m", function(c)
-			c.maximized_vertical = not c.maximized_vertical
-			c:raise()
-		end, { description = "(un)maximize vertically", group = "client" }),
-		awful.key({ modkey, shift }, "m", function(c)
-			c.maximized_horizontal = not c.maximized_horizontal
-			c:raise()
-		end, { description = "(un)maximize horizontally", group = "client" }),
-
-		-- Client position management
-		awful.key({ modkey }, "t", function(c)
-			c.ontop = not c.ontop
-		end, { description = "toggle keep on top", group = "client" }),
-		awful.key({ modkey }, "right", function(c)
-			local f = awful.placement.scale + awful.placement.right + awful.placement.maximize_vertically
-			f(c, { honor_workarea = true, to_percent = 0.5 })
-		end, { description = "snap client right", group = "floating client" }),
-		awful.key({ modkey }, "left", function(c)
-			local f = awful.placement.scale + awful.placement.left + awful.placement.maximize_vertically
-			f(c, { honor_workarea = true, to_percent = 0.5 })
-		end, { description = "snap client left", group = "floating client" }),
-		awful.key({ modkey }, "up", function(c)
-			local f = awful.placement.scale + awful.placement.top + awful.placement.maximize_horizontally
-			f(c, { honor_workarea = true, to_percent = 0.5 })
-		end, { description = "snap client top", group = "floating client" }),
-		awful.key({ modkey }, "down", function(c)
-			local f = awful.placement.scale + awful.placement.bottom + awful.placement.maximize_horizontally
-			f(c, { honor_workarea = true, to_percent = 0.5 })
-		end, { description = "snap client bottom", group = "floating client" }),
-
-		-- Client Screen management
-		awful.key({ modkey }, "o", function(c)
-			c:move_to_screen()
-			awful.client.jumpto(c)
-		end, { description = "move to screen", group = "client" }),
-	})
+	awful.keyboard.append_client_keybindings(helper(client_bindings))
 end)
