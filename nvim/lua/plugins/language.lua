297a44f6f3d25ae1c110b488b0fae4d46e8bb689
Nil Omniscient <nilomniscient@gmail.com>
2026-09-18 15:02:00 -0400

    Adding nvim-c3

--- nvim/lua/plugins/language.lua
+++ nvim/lua/plugins/language.lua
@@ -8,4 +8,12 @@ return {
 		},
 		config = true,
 	},
+	{
+		"ManuLinares/nvim-c3",
+		ft = { "c3", "c3i", "c3t" },
+		build = function()
+			require("c3").update()
+		end,
+		config = true,
+	},
 }
